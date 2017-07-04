aa1101972b9aa8d7afe048a7a9dd7d196cd37422
Chanse Campbell <chansecampbell@gmail.com>
2017-07-04 12:06:32 +0100

    Now able to convert the user inputted location and turn it into something that should work with the weather website

--- whats-the-weather/ViewController.swift
+++ whats-the-weather/ViewController.swift
@@ -10,6 +10,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var textField: UITextField!
+    @IBOutlet var weatherLabel: UILabel!
+    
+    @IBAction func submitButton(_ sender: Any) {
+        buildUrl()
+    }
+    
+    func buildUrl() {
+        let userUrl = textField.text
+        let url = userUrl?.replacingOccurrences(of: " ", with: "-")
+        print("User submitted " + url!)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
